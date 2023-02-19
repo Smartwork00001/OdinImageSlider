e4d330a127e93b8cee3d4d21f4f02abb4045ed6a
Teibok Chyne <smartwork00001@gmail.com>
2023-02-19 11:17:38 +0530

    Create navigation bar
    Navigation is faulty when moving left due to hardcoding the place of images on HTML

--- src/GlobalVariables.js
+++ src/GlobalVariables.js
@@ -1,11 +1,55 @@
 class GlobalVariables{
     constructor(){
         this.imageNumber = 1;
+        this.navigationLeft = 0;
+        this.navigationRight = 3;
+        this.navigationArr = [1,2,3,4];
         this.imageTotal = 18;
+    }
+
+    getNavigationArr(){
+        return this.navigationArr;
+    }
+
+    navigationLeftShift(){
+        for(let i=0;i<this.navigationArr.length;i+=1){
+            this.navigationArr[i] = this.prevOf(this.navigationArr[i]);
+        }
+    }
+
+    navigationRightShift(){
+        for(let i=0;i<this.navigationArr.length;i+=1){
+            this.navigationArr[i] = this.nextOf(this.navigationArr[i]);
+        }
+    }
+
+    prevOf(num){
+        if(num === 0){
+            return this.imageTotal-1;
+        }
+        return num-1;
+    }
+
+    nextOf(num){
+        if(num === this.imageTotal - 1){
+            return 0;
+        }
+        return num+1;
+    }
+
+    generateNavigationArr(num){
+        this.navigationArr[0] = num;
+        this.navigationArr[1] = this.nextOf(this.navigationArr[0]);
+        this.navigationArr[2] = this.nextOf(this.navigationArr[1]);
+        this.navigationArr[3] = this.nextOf(this.navigationArr[2]);
     }
 
     getImageNumber(){
         return this.imageNumber;
+    }
+
+    getImageTotal(){
+        return this.imageTotal;
     }
 
     setImageNumber(num){
@@ -24,6 +68,48 @@ class GlobalVariables{
             return 0;
         }
         return this.imageNumber+1;
+    }
+
+    getNavigationLeft(){
+        return this.navigationLeft;
+    }
+
+    getNavigationRight(){
+        return this.navigationRight;
+    }
+
+    setNavigationLeft(num){
+        this.navigationLeft = num;
+    }
+
+    setNavigationRight(num){
+        this.navigationRight = num;
+    }
+
+    navigationNext(){
+        if(this.navigationLeft === this.imageTotal -1){
+            this.navigationLeft = 0;
+        }else{
+            this.navigationLeft +=1; 
+        }
+        if(this.navigationRight === this.imageTotal - 1){
+            this.navigationRight = 0;
+        }else{
+            this.navigationRight +=1;
+        }
+    }
+
+    navigationPrev(){
+        if(this.navigationLeft === 0){
+            this.navigationLeft = this.imageTotal-1;
+        }else{
+            this.navigationLeft -=1; 
+        }
+        if(this.navigationRight === 0){
+            this.navigationRight = this.imageTotal-1;
+        }else{
+            this.navigationRight -=1;
+        }
     }
 };
 
--- src/buttons.js
+++ src/buttons.js
@@ -11,35 +11,71 @@ const navigationSlideLeftButton = document.querySelector(
 
 const globalVariables = new GlobalVariables();
 
-const showNextImage = function(){
-    const currImg = document.querySelector(`#i${globalVariables.getImageNumber()}`);
-    const nextImgNumber = globalVariables.getNextImageNumber();
-    const nextImg = document.querySelector(`#i${nextImgNumber}`);
-    currImg.classList.add("hidden");
-    nextImg.classList.remove("hidden");
-    globalVariables.setImageNumber(nextImgNumber);
-}
-
-const showPrevImage = function(){
-    const currImg = document.querySelector(`#i${globalVariables.getImageNumber()}`);
-    const prevImgNumber = globalVariables.getPrevImageNumber();
-    const prevImg = document.querySelector(`#i${prevImgNumber}`);
-    currImg.classList.add("hidden");
-    prevImg.classList.remove("hidden");
-    globalVariables.setImageNumber(prevImgNumber);
-}
-
-const keyDownHandler = function(event){
-    if(event.keyCode === 39){
-        showNextImage();
-    }else if(event.keyCode === 37){
-        showPrevImage();
+const navigationHandler = function () {
+  let arr = globalVariables.getNavigationArr();
+  const imageNumber = globalVariables.getImageNumber();
+  let flag = false;
+  arr.forEach((x) => {
+    if (x === imageNumber) {
+      flag = true;
     }
-}
+  });
+  if (!flag) {
+    globalVariables.generateNavigationArr(globalVariables.getImageNumber());
+    arr = globalVariables.getNavigationArr();
+    console.log(arr[0],arr[1],arr[2],arr[3]);
+    for (let i = globalVariables.getImageTotal() - 1; i >= 0; i -= 1) {
+      const div = document.querySelector(`.navigation#i${i}`);
+      if (!div.classList.contains("hidden")) {
+        div.classList.add("hidden");
+      }
+    }
+    for (let i = arr.length - 1; i >= 0; i -= 1) {
+      const div = document.querySelector(`.navigation#i${arr[i]}`);
+      if (div.classList.contains("hidden")) {
+        div.classList.remove("hidden");
+      }
+    }
+  }
+};
+
+const showNextImage = function () {
+  const currImg = document.querySelector(
+    `.slide#i${globalVariables.getImageNumber()}`
+  );
+  const nextImgNumber = globalVariables.getNextImageNumber();
+  const nextImg = document.querySelector(`.slide#i${nextImgNumber}`);
+  currImg.classList.add("hidden");
+  nextImg.classList.remove("hidden");
+
+  globalVariables.setImageNumber(nextImgNumber);
+  navigationHandler();
+};
+
+const showPrevImage = function () {
+  const currImg = document.querySelector(
+    `#i${globalVariables.getImageNumber()}`
+  );
+  const prevImgNumber = globalVariables.getPrevImageNumber();
+  const prevImg = document.querySelector(`#i${prevImgNumber}`);
+  currImg.classList.add("hidden");
+  prevImg.classList.remove("hidden");
+
+  globalVariables.setImageNumber(prevImgNumber);
+  navigationHandler();
+};
+
+const keyDownHandler = function (event) {
+  if (event.keyCode === 39) {
+    showNextImage();
+  } else if (event.keyCode === 37) {
+    showPrevImage();
+  }
+};
 
 const imageSliderHandler = function (event) {
   if (event.target.classList.contains("slide-left-button")) {
-   showPrevImage();
+    showPrevImage();
   } else {
     showNextImage();
   }
@@ -49,4 +85,4 @@ slideLeftButton.addEventListener("click", imageSliderHandler);
 slideRightButton.addEventListener("click", imageSliderHandler);
 navigationSlideLeftButton.addEventListener("click", imageSliderHandler);
 navigationSlideRightButton.addEventListener("click", imageSliderHandler);
-window.addEventListener('keydown',keyDownHandler);
+window.addEventListener("keydown", keyDownHandler);
